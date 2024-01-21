f867c3c1c886bde7c03657810e6fd32c1a2cf200
DevValenzuela <49131215+DevValenzuela@users.noreply.github.com>
2024-01-21 14:47:01 -0500

    Update Ciclo Live Swift

--- CicloAppSwift/ViewController.swift
+++ CicloAppSwift/ViewController.swift
@@ -8,11 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        print("Se activo el loadView")
+        // Se activa caundo iniciamos una app y la va a cargar
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Se activo el viewLoad")
+        // Se activa cuando la app ya cargo en memoria
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Se activo el viewWillApper")
+        // Se activa cuando la vista esta a punto de ser visible; Aqui ya la vista ya Cargo...
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Se activo el viewDidApper")
+        // Se activa cuando la vista ya es visible por el usuario
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Se activo el viewWillDisappear")
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Se activo el viewDidDisaAppear")
+        // Se activa cuando la vista desapareio
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print("Se activo el didReceiveMemoryWarning")
+        // Se activa cuando el dispositivo se queda sin memoria
+    }
+    
+    
 
 
 }
